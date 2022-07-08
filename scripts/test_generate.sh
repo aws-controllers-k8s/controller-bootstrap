2051f90a6399ca189e742bfd61231dd0e3ae0b49
James Park <jamesjhp@amazon.com>
2022-07-08 14:57:26 -0700

    add end-to-end tests for controller-bootstrap
    fix code implementation

---
+++ scripts/test_generate.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CONTROLLER_BOOTSTRAP_DIR="$SCRIPTS_DIR/.."
+CONTROLLER_BOOTSTRAP="$CONTROLLER_BOOTSTRAP_DIR/bin/controller-bootstrap"
+CONTROLLER_BOOTSTRAP_TEMPLATES_DIR="$CONTROLLER_BOOTSTRAP_DIR/templates"
+SERVICE="eks"
+CONTROLLER_NAME="$SERVICE-controller"
+CONTROLLER_DIR="$CONTROLLER_BOOTSTRAP_DIR/test_output/$CONTROLLER_NAME"
+
+export SERVICE
+export CONTROLLER_DIR
+cd $CONTROLLER_BOOTSTRAP_DIR
+make generate
+
+if [[ ! -e "$CONTROLLER_DIR/TEST_FILE" ]]; then
+  echo "'TEST_FILE' is not found in $CONTROLLER_NAME. Creating a new template test file, controller-bootstrap/templates/TEST_FILE.tpl ..."
+  cd "$CONTROLLER_BOOTSTRAP_TEMPLATES_DIR"
+  touch "TEST_FILE.tpl"
+  rm -rf ${CONTROLLER_DIR}
+  echo "Generating $CONTROLLER_NAME using command 'make generate'"
+  cd "$CONTROLLER_BOOTSTRAP_DIR"
+  make generate
+fi
+
+if [[ ! -e "$CONTROLLER_DIR/TEST_FILE" ]]; then
+  echo "Unable to find 'TEST_FILE' in $CONTROLLER_NAME. Failed to generate the controller. Exiting"
+  exit 1
+else
+  echo "Generated successfully and found 'TEST_FILE' in $CONTROLLER_NAME"
+fi
+
+cd "$CONTROLLER_BOOTSTRAP_TEMPLATES_DIR"
+rm "TEST_FILE.tpl"
---
+++ scripts/test_update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CONTROLLER_BOOTSTRAP_DIR="$SCRIPTS_DIR/.."
+CONTROLLER_BOOTSTRAP="$CONTROLLER_BOOTSTRAP_DIR/bin/controller-bootstrap"
+NOTICE_TPL_FILE="$CONTROLLER_BOOTSTRAP_DIR/templates/NOTICE.tpl"
+AWS_SERVICE="eks"
+CONTROLLER_NAME="$AWS_SERVICE-controller"
+OUTPUT_DIR="$CONTROLLER_BOOTSTRAP_DIR/test_output"
+CONTROLLER_DIR="$OUTPUT_DIR/$CONTROLLER_NAME"
+
+if ! grep -wq -- "this line is added by ACK update test" "$CONTROLLER_DIR/NOTICE"; then
+    echo "Unable to find 'this line is added by ACK update test' in the 'NOTICE' file of $CONTROLLER_NAME. Adding 'this line is added by ACK update test' in controller-bootstrap/templates/NOTICE.tpl file ..."
+    echo "this line is added by ACK update test" >> $NOTICE_TPL_FILE
+    echo "Updating the project description files in $CONTROLLER_NAME"
+    cd $CONTROLLER_BOOTSTRAP_DIR
+    make build
+    ${CONTROLLER_BOOTSTRAP} update --aws-service-alias ${AWS_SERVICE} --output-path ${CONTROLLER_DIR}
+fi
+
+if ! grep -wq -- "this line is added by ACK update test" "$CONTROLLER_DIR/NOTICE"; then
+    echo "Unable to find 'this line is added by ACK update test' in the 'NOTICE' file. Failed to update the project description files in $CONTROLLER_NAME. Exiting"
+    rm -rf ${OUTPUT_DIR}
+    exit 1
+else
+    echo "Updated successfully and found 'this line is added by ACK update test' in the 'NOTICE' file of $CONTROLLER_NAME"
+fi
+
+sed -i '' "/this line is added by ACK update test/d" $NOTICE_TPL_FILE
+rm -rf ${OUTPUT_DIR}
