99ad93195c0d270ad36b071ab344bbf3f8bef880
James Park <jamesjhp@amazon.com>
2022-07-11 09:49:04 -0700

    make text to find into a variable

--- scripts/test_update.sh
+++ scripts/test_update.sh
@@ -10,23 +10,24 @@ AWS_SERVICE="eks"
 CONTROLLER_NAME="$AWS_SERVICE-controller"
 OUTPUT_DIR="$CONTROLLER_BOOTSTRAP_DIR/test_output"
 CONTROLLER_DIR="$OUTPUT_DIR/$CONTROLLER_NAME"
+TEXT_TO_FIND="'this line is added by ACK update test'"
 
-if ! grep -wq -- "this line is added by ACK update test" "$CONTROLLER_DIR/NOTICE"; then
-    echo "Unable to find 'this line is added by ACK update test' in the 'NOTICE' file of $CONTROLLER_NAME. Adding 'this line is added by ACK update test' in controller-bootstrap/templates/NOTICE.tpl file ..."
-    echo "this line is added by ACK update test" >> $NOTICE_TPL_FILE
+if ! grep -wq -- "$TEXT_TO_FIND" "$CONTROLLER_DIR/NOTICE"; then
+    echo "Unable to find $TEXT_TO_FIND in the 'NOTICE' file of $CONTROLLER_NAME. Adding $TEXT_TO_FIND in controller-bootstrap/templates/NOTICE.tpl file ..."
+    echo "$TEXT_TO_FIND" >> $NOTICE_TPL_FILE
     echo "Updating the project description files in the existing $CONTROLLER_NAME"
     cd "$CONTROLLER_BOOTSTRAP_DIR"
     make build
     ${CONTROLLER_BOOTSTRAP} update --aws-service-alias ${AWS_SERVICE} --output-path ${CONTROLLER_DIR}
 fi
 
-if ! grep -wq -- "this line is added by ACK update test" "$CONTROLLER_DIR/NOTICE"; then
-    echo "Unable to find 'this line is added by ACK update test' in the 'NOTICE' file. Failed to update the project description files in $CONTROLLER_NAME. Exiting"
+if ! grep -wq -- "$TEXT_TO_FIND" "$CONTROLLER_DIR/NOTICE"; then
+    echo "Unable to find "$TEXT_TO_FIND" in the 'NOTICE' file. Failed to update the project description files in $CONTROLLER_NAME. Exiting"
     rm -rf ${OUTPUT_DIR}
     exit 1
 else
-    echo "Updated successfully and found 'this line is added by ACK update test' in the 'NOTICE' file of $CONTROLLER_NAME"
+    echo "Updated successfully and found "$TEXT_TO_FIND" in the 'NOTICE' file of $CONTROLLER_NAME"
 fi
 
-sed -i '' "/this line is added by ACK update test/d" $NOTICE_TPL_FILE
+sed -i '' "/$TEXT_TO_FIND/d" $NOTICE_TPL_FILE
 rm -rf ${OUTPUT_DIR}
